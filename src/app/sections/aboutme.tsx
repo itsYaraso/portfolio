f8e0426b4ea6ae72855d4d7236417266b84892c8
itsYaraso <benshigregg@gmail.com>
2024-09-03 04:13:52 +0300

    z-index readjustment

--- src/app/sections/aboutme.tsx
+++ src/app/sections/aboutme.tsx
@@ -5,13 +5,13 @@ export default function aboutMeSection() {
       <p className="text-5xl mb-4 text-center">About me</p>
       <hr className="mx-auto pb-4"></hr>
 
-      <p className="text-sm text-center break-normal text-balance">
+      <p className="text-sm text-center break-normal text-balance z-0">
         <br></br>
         I am 31, father of 3 based in Finland, originating from Manchester, United Kingdom.
         <br></br>
         <br></br>
         I like figuring out and solving puzzles no matter how difficult it may be it, it is
-        always excilirating when you have solved something you have never tried before.
+        always exhilarating when you have solved something you have never tried before.
         <br></br>
         Being a self taught developer has had it's challenges
         but this allows me to come at and tackle problems from a different perspective allowing me to try new approaches and options
