8a6e2d99d495286228664ddb6af2073da98abd1e
itsYaraso <benshigregg@gmail.com>
2024-09-03 20:15:25 +0300

    beta release requiring bug reports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,8 @@ export default function Home() {
 
       <section className="pb-36">
         <Timeline />
+        <p className="text-2xl mt-8">ERROR 404 EXPERIENCE NOT FOUND</p>
+        <img src="avatar.png" className="md:h-80 md:w-80 rounded-full ml-14" alt="avatar" />
       </section>
 
       <section className="pb-36">
