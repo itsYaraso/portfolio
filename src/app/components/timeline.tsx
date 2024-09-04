e83f82e5d3532d9abac16a95209a4bb7045f2892
itsYaraso <benshigregg@gmail.com>
2024-09-04 16:39:22 +0300

    experience adjustment spacing

--- src/app/components/timeline.tsx
+++ src/app/components/timeline.tsx
@@ -1,7 +1,7 @@
 export default function Timeline() {
   return (
     <div className="">
-      <p className="text-5xl mb-4 text-center p-4">Experience </p>
+      <p className="text-5xl text-center p-4">Experience </p>
       <hr className=""></hr>
     </div>
   )
