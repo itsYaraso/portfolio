a47143187b9c9cee13afe817596ccf007c1243fb
itsYaraso <benshigregg@gmail.com>
2024-09-09 23:11:20 +0300

    social section sizing fix

--- src/app/sections/socialsection.tsx
+++ src/app/sections/socialsection.tsx
@@ -8,7 +8,7 @@ export default function socialSection() {
           <a className="" href="https://github.com/itsYaraso" target="_blank">
             <img
               src="/github.svg"
-              className="cursor-pointer hover:scale-150 ease-in-out duration-200 2xl:w-2/4"
+              className="cursor-pointer hover:scale-150 ease-in-out duration-200"
               width={24}
               height={24}
               alt="itsYaraso"
@@ -18,14 +18,14 @@ export default function socialSection() {
           <a className="" href="https://www.linkedin.com/in/benjamin-gregg-9a514397/" target="_blank">
             <img
               src="/linkedin.svg"
-              className="invert cursor-pointer hover:scale-150 ease-in-out duration-200 2xl:w-5/6"
+              className="invert cursor-pointer hover:scale-150 ease-in-out duration-200"
               width={24}
               height={24}
               alt="Linkedin"
             />
           </a>
             <a href="mailto:benshigregg@gmail.com" target="_blank">
-                <Mail className="cursor-pointer hover:scale-150 ease-in-out duration-200 2xl:w-5/6"/>
+                <Mail className="cursor-pointer hover:scale-150 ease-in-out duration-200"/>
             </a>
         </div>
       </div>
