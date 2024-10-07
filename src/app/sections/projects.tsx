00ff160eda3abf9ee3d9d156288de27d0548d95c
itsYaraso <benshigregg@gmail.com>
2024-10-07 21:02:52 +0300

    centered button for projects

--- src/app/sections/projects.tsx
+++ src/app/sections/projects.tsx
@@ -9,7 +9,7 @@ export default function projectsSection() {
 
             <div className="group mb-6">
                 <span className="w-full gap-2 px-3 py-2 border border-white rounded-md bg-neutral-50 bg-opacity-5 border-opacity-10 p-4 text-center">
-                    <a href="https://github.com/itsYaraso/todotome" target="_blank"> <button>ToDoToMe</button> </a>
+                    <a href="https://github.com/itsYaraso/todotome" target="_blank"> <button className="flex justify-center">ToDoToMe</button> </a>
                 </span>
                 <span className="group-hover:opacity-100 transition-opacity
                  bg-neutral-900 px-1 border rounded-md absolute opacity-0 m-8 mx-auto text-wrap max-w-xs"
